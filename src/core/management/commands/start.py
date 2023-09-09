0b3bf610f07376e071804af65667571c388f7ffa
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-10 02:20:36 +0330

    chore: add `start` management command

---
+++ src/core/management/commands/start.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create required configs/models before first run'
+
+    def handle(self, *args, **options):
+        import os
+        os.remove("./db.sqlite3")
+        os.remove("./podcast/migrations/0001_initial.py")
+        os.system("python manage.py makemigrations podcast")
+        os.system("python manage.py migrate")
+        user = User.objects.create_superuser(username="admin", email="admin@admin.admin", password="admin")
