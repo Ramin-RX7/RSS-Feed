118fa9d51f2a3cb75855ba0e4559bec58c97729b
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-11 02:13:43 +0330

    add get_rss content to parsers

--- src/core/parser/xml_parser.py
+++ src/core/parser/xml_parser.py
@@ -6,3 +6,19 @@ from core import xmltodict
 
 
 def get_rss_content(rss_object) -> dict:
+    url = rss_object.url
+    response = requests.get(url)
+
+    o = xmltodict.parse(response.text)
+    main_content = o["rss"]["channel"]
+
+    return main_content
+
+def get_rss_main_content(rss_object):
+    full_content = get_rss_content(rss_object)
+    if full_content.get("item"):
+        full_content.pop("item")
+    return full_content
+
+
+
