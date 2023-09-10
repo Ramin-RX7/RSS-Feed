0acd13869f2916ec50971709a9532649528797c0
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-11 02:12:47 +0330

    feat: PodcastEpisodePaths model

--- src/podcasts/models.py
+++ src/podcasts/models.py
@@ -1,3 +1,28 @@
+from django.db import models
+
+from core.models import BaseModel
+from core.parser import *
+
+
+
+
+class PodcastEpisodePaths(models.Model):
+    audio_file_path = models.CharField(max_length=100)
+    duration_path = models.CharField(max_length=100)
+    title_path = models.CharField(max_length=100)
+    duration = models.CharField(max_length=100)
+    audio_file = models.CharField(max_length=100)
+    publish_date = models.CharField(max_length=100)
+
+    explicit = models.CharField(max_length=100)
+    summary = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    guests = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+
+
+
 class PodcastRSSPaths(models.Model):
     title = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
