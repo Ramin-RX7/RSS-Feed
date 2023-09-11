fa8cee297f412c02832191791a950d92d5fbdfa0
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-12 03:12:36 +0330

    feat: add EpisodeListView

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from rest_framework import generics
+
+
+
+
+class EpisodeListView(generics.ListAPIView):
+    model = None
+    def get_queryset(self):
+        rss_id = self.kwargs['rss_pk']
+        queryset = self.model.objects.filter(rss__id=rss_id)
+        return queryset
+
