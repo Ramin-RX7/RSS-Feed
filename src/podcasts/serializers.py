7232a88f06abab17ea67110aba3de1db0df8654d
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-12 03:14:07 +0330

    feat: add PodcastRSSSerializer

--- src/podcasts/serializers.py
+++ src/podcasts/serializers.py
@@ -10,3 +10,36 @@ class MainFieldsSerializer(ModelSerializer):
         model = PodcastMainFields
         exclude = ("id",)
 
+
+
+class PodcastRSSSerializer(ModelSerializer):
+    # main_fields = MainFieldsSerializer()
+    # field1 = serializers.CharField(source='main_fields.title')
+
+    class Meta:
+        model = PodcastRSS
+        exclude = (
+            "id",
+            "name",
+            "url",
+            "created_at",
+            "updated_at",
+            "main_fields",
+            "episode_attributes_path",
+            "main_fields_path",
+        )
+
+
+    def to_representation(self, instance):
+        main_fields_serializer = MainFieldsSerializer(instance.main_fields)
+        rss_data = super().to_representation(instance)
+        rss_data.update(main_fields_serializer.data)
+        return rss_data
+        # representation = super().to_representation(instance)
+        # main_field_serializer = MainFieldsSerializer(instance.main_fields)
+        # for field in main_field_serializer.get_fields():
+            # representation[field] = main_field_serializer.data[field]
+        # return representation
+
+
+
