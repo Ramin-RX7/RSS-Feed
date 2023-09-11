ad84d063e0f370ca0ddfbc3d7784d50c012b5f79
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-11 23:48:26 +0330

    feat: add Subscribe model

--- src/interactions/models.py
+++ src/interactions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+from accounts.models import User
+from podcasts.models import PodcastRSS,PodcastEpisode
+
+
+
+
+class Subscribe(BaseModel):
+    user = models.ForeignKey(User)
+    rss = models.ForeignKey(PodcastRSS)
+
+
+
