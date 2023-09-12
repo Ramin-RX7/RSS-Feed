cf3943fb72b98764f77c886ecb574eeaae7856e5
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-12 15:16:54 +0330

    feat: add create_new_episode view

--- src/podcasts/views.py
+++ src/podcasts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from rest_framework import generics
 
 
@@ -22,7 +22,7 @@ class PodcastListView(generics.ListAPIView):
     queryset = PodcastRSS.objects.all()
     serializer_class = PodcastRSSSerializer
 
-class PodcastView(generics.RetrieveAPIView):
+class PodcastDetailView(generics.RetrieveAPIView):
     queryset = PodcastRSS.objects.all()
     serializer_class = PodcastRSSSerializer
 
@@ -37,3 +37,12 @@ class PodcastEpisodeListView(EpisodeListView):
 class PodcastEpisodeDetailView(EpisodeDetailView):
     queryset = PodcastEpisode.objects.all()
     serializer_class = PodcastEpisodeSerializer
+
+
+
+def create_new_episode(request, rss_pk, episode_nom):
+    rss = PodcastRSS.objects.get(id=rss_pk)
+    parser = EpisodeXMLParser(rss, PodcastEpisode)
+    content = get_rss_content(rss)
+    parser.create_new_episode(content["item"][episode_nom-1])
+    return HttpResponse("done")
