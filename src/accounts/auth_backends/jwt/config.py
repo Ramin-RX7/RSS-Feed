2025c859473ceb91b9a69f383ca2b29f8f8151f7
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-13 17:20:53 +0330

    fix: add timedelta for access/refresh token expiry token

--- src/accounts/auth_backends/jwt/config.py
+++ src/accounts/auth_backends/jwt/config.py
@@ -1,6 +1,10 @@
+from datetime import datetime,timedelta
 from django.conf import settings as SETTINGS
+
+__all__ = ("SECRET_KEY", "ENCRYPTION", "ACCESS_TOKEN_EXPIRY", "REFRESH_TOKEN_EXPIRY")
 
 SECRET_KEY = SETTINGS.SECRET_KEY
 ENCRYPTION = 'HS256'
-ACCESS_TOKEN_EXPIRY = 60*60*24  # one day in seconds
-REFRESH_TOKEN_EXPIRY = ACCESS_TOKEN_EXPIRY * 5
+_access_token_expiry = timedelta(seconds=60*60*24) # one day in seconds
+ACCESS_TOKEN_EXPIRY = datetime.utcnow() + _access_token_expiry
+REFRESH_TOKEN_EXPIRY = datetime.utcnow() + _access_token_expiry*5
