50e44537ca951f72a4b72987a40f2d6b0d09926c
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-13 17:01:58 +0330

    feat: add UserRegisterView

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.core.cache import caches
+from rest_framework import status, permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import exceptions
 
-# Create your views here.
+from .serializers import UserRegisterSerializer, UserLoginSerializer
+from .auth_backends import LoginAuthBackend, JWTAuthBackend
+from .auth_backends.jwt.utils import generate_tokens, decode_jwt
+from .models import User
+
+
+
+
+
+class UserRegisterView(CreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = UserRegisterSerializer
+
+
