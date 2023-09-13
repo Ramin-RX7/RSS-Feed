e2bf2d303b626f99402712f2bcb628df030c0047
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-13 17:25:15 +0330

    config: set up the cache backends

--- src/config/settings.py
+++ src/config/settings.py
@@ -99,7 +99,25 @@ DATABASES = {
     'default': env.db(),
 }
 CACHES = {
-    "default": env.cache()
+    # "default": env.cache(),
+    # "auth": env.cache_url("AUTH_CACHE"),
+    # 'default': {
+    #     'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    #     'LOCATION': 'unique-snowflake',  # Unique cache identifier
+    # },
+    # 'auth': {
+    #     'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    #     'LOCATION': 'auth-cache',  # Unique cache identifier for auth cache
+    # },
+    "default" : {
+        "BACKEND":"django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0"
+    },
+    "auth" : {
+        "BACKEND":"django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "TIMEOUT": 300
+    },
 }
 
 
