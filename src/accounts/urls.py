840e1321209a950b77f160ea2c65dd7a094ffcc0
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-14 03:53:23 +0330

    chore: add token_refresh url

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/refresh/', views.RefreshToken.as_view(), name='token_refresh'),
     path('hi/', views.JWTAuthTestView.as_view(), name='jwt-auth-test'),
 ]
