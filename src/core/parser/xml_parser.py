485070257daca020fea85167f23b1252b4f8bd64
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-14 13:01:13 +0330

    refactor: move parser utility functions to utils.py

--- src/core/parser/xml_parser.py
+++ src/core/parser/xml_parser.py
@@ -1,31 +1,6 @@
-import requests
-
 from django.db.models import Model
 
-from core.lib import xmltodict
-
-
-
-
-def get_rss_content(rss_object) -> dict:
-    url = rss_object.url
-    response = requests.get(url)
-
-    o = xmltodict.parse(response.text)
-    main_content = o["rss"]["channel"]
-
-    return main_content
-
-def get_rss_main_content(rss_object):
-    full_content = get_rss_content(rss_object)
-    # if full_content.get("item"):
-    full_content.pop("item")
-    return full_content
-
-def get_rss_episodes(rss_object):
-    full_content = get_rss_content(rss_object)
-    return full_content.get("item")
-
+from .utils import *
 
 
 
