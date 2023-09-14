86d5c65fef20a8e8ef5dc6104b27698ea197702f
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-14 03:54:04 +0330

    chore: remove extra code in UserLoginView

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -4,16 +4,16 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
-from rest_framework import exceptions
 
 from .serializers import UserRegisterSerializer, UserLoginSerializer
 from .auth_backends import LoginAuthBackend, JWTAuthBackend
-from .auth_backends.jwt.utils import generate_tokens, decode_jwt
-from .models import User
+from .auth_backends.jwt.utils import generate_tokens,decode_jwt,_save_cache,_get_user_agent
 
 
 
 auth_cache = caches["auth"]
+
+
 
 
 
@@ -28,6 +28,7 @@ class UserLoginView(APIView):
     serializer_class = UserLoginSerializer
 
     def post(self, request):
+        user_agent = _get_user_agent(request.headers)
 
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -39,25 +40,14 @@ class UserLoginView(APIView):
             return Response({'message': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
         jti,access_token,refresh_token = generate_tokens(username)
-        user_agent = self._get_user_agent(request.headers)
-        key = f"{user.id}|{jti}"
-        value = f"{user_agent}"
 
-        print(jti)
-        auth_cache.set(key,value)
-        # print(auth_cache.get(key))
+        _save_cache(user, jti, user_agent)
 
         data = {
             "access": access_token,
             "refresh": refresh_token,
         }
         return Response(data, status=status.HTTP_201_CREATED)
-
-    def _get_user_agent(self, headers):
-        user_agent = headers.get("user-agent")
-        if user_agent is None:
-            raise exceptions.ValidationError('user-agent header is not provided')
-        return user_agent
 
 
 
