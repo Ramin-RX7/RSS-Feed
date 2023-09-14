17d8f02ebc774f081bb834b2cd6440691272cde3
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-14 15:06:36 +0330

    feat: add update_episodes function

--- src/core/parser/update.py
+++ src/core/parser/update.py
@@ -10,3 +10,18 @@ def update_rss(rss_object:PodcastRSS):
     rss.save()
 
 
+def update_episodes(rss_object:PodcastRSS, episodes_model:PodcastEpisode):
+    live_episodes = get_rss_episodes(rss_object)
+    db_episode = episodes_model.objects.filter(rss=rss_object)
+
+    diff = len(live_episodes) > db_episode.count()
+    print(diff)
+    if diff > 0:
+        unparsed_episodes = live_episodes[:diff]
+        parser = EpisodeXMLParser()
+        new_episode_objects = parser.parse_multiple_episodes(unparsed_episodes)
+        episodes_model.objects.bulk_create(new_episode_objects)
+    elif diff == 0:
+        print("Up to date")
+        return
+    else: raise SystemError("How?! (This happens when older episodes get deleted)")
