1ef23d3e304bc1e6549b563ba0adcb03c361db10
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-14 04:15:12 +0330

    chore: add logout url

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/refresh/', views.RefreshToken.as_view(), name='token_refresh'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('hi/', views.JWTAuthTestView.as_view(), name='jwt-auth-test'),
 ]
