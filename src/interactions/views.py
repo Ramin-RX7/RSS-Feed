2fd0ae7aa617d41e3f7e6e6e1a75675381c01a95
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-19 22:48:12 +0330

    feat: add LikeView

--- src/interactions/views.py
+++ src/interactions/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from accounts.auth_backends import JWTAuthBackend
+from podcasts.models import PodcastEpisode
+from .serializers import *
+
+
+
+
+
+class LikeView(generics.CreateAPIView):
+    authentication_classes = (JWTAuthBackend,)
+    permission_classes = (IsAuthenticated,)
+
+    serializer_class = LikeSerializer
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        episode_id = request.data.get('episode_id')
+
+        try:
+            existing_like = Like.objects.get(user=user, episode=episode_id)
+            existing_like.delete()
+            return Response({'detail': 'Like removed successfully.'}, status=status.HTTP_200_OK)
+        except Like.DoesNotExist:
+            like_data = {'user': user.id, 'episode': episode_id}
+            serializer = self.get_serializer(data=like_data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
