384a02f69d2c8be01fe1ecb7fe806cc1e768a10f
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-25 08:58:43 +0330

    build: add celery broker and backend to settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -185,3 +185,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
 
+
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
