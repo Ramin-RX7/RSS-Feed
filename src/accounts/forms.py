47659f8dc9b82dd38c2575f69f877e009049b060
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-26 23:47:02 +0330

    feat: add User ChangeForm

---
+++ src/accounts/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import UserChangeForm,BaseUserCreationForm
+
+from .models import User
+
+
+
+class ChangeForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(), required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', "is_superuser", "email", 'first_name', 'last_name', 'is_staff', 'is_active','groups','user_permissions','password', )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].required = False
+        self.fields['last_name'].required = False
+
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data['password']
+        if password:
+            user.set_password(password)
+        # elif not password and user.pk:
+        #     old_user = User.objects.get(pk=user.pk)
+        #     user.password = old_user.password
+        if commit:
+            user.save()
+        return user
+
