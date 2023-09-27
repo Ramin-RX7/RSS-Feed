5af726dce499fe035ffd97e5d1c544acd24650e5
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-27 14:55:42 +0330

    chore: add password reset urls

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/refresh/', views.RefreshTokenView.as_view(), name='token_refresh'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('change-password/', views.ChangePassword.as_view(), name="change_password"),
+    path('reset-password/<str:code>', views.ResetPassword.as_view({"get":"get", "post":"post"}), name="reset_password"),
+    path('reset-password/', views.ResetPassword.as_view({"post":"reset_password_request"}), name="reset_password_request"),
 ]
