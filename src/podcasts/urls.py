8284b4076ad827da6b3c4ad8181c616b3f0f980c
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-29 00:05:18 +0330

    chore: recommendation url multiple method choice

--- src/podcasts/urls.py
+++ src/podcasts/urls.py
@@ -14,7 +14,7 @@ urlpatterns = format_suffix_patterns([
     path('podcast/<int:rss_pk>/episodes/', PodcastEpisodeListView.as_view()),
     path('podcast/<int:rss_pk>/episode/<int:episode_nom>', PodcastEpisodeDetailView.as_view()),
 
-    path('recommended/', PodcastRecommendationView.as_view()),
+    path('recommended/<str:method>/', PodcastRecommendationView.as_view()),
 
     # re_path(r'podcast/test/(?P<pk>\d*)', PodcastTest.as_view(),),
     # re_path(r'podcast/test/(?P<episode_nom>\d*)', PodcastTest.as_view(),),
