41742ecad50a5a5829c0f9b311974ed73104952e
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-28 16:37:15 +0330

    chore: add podcast recommendation url

--- src/podcasts/urls.py
+++ src/podcasts/urls.py
@@ -14,7 +14,7 @@ urlpatterns = format_suffix_patterns([
     path('podcast/<int:rss_pk>/episodes/', PodcastEpisodeListView.as_view()),
     path('podcast/<int:rss_pk>/episode/<int:episode_nom>', PodcastEpisodeDetailView.as_view()),
 
-    path('podcast/<int:rss_pk>/episodes/create/<int:episode_nom>', create_new_episode),
+    path('recommended/', PodcastRecommendationView.as_view()),
 
     # re_path(r'podcast/test/(?P<pk>\d*)', PodcastTest.as_view(),),
     # re_path(r'podcast/test/(?P<episode_nom>\d*)', PodcastTest.as_view(),),
