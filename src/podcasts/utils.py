4d0f9495ba75d53c123df24c7ccfe2fdf3e240c5
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-29 00:00:02 +0330

    feat: add subscription based recommendation

--- src/podcasts/utils.py
+++ src/podcasts/utils.py
@@ -16,14 +16,18 @@ def get_categories_count(podcasts):
 
 
 
+
+def subscription_based_recommended_podcasts(user):
     all_podcasts = PodcastRSS.objects.all()
     user_podcasts = all_podcasts.filter(subscribe__user=user)
-    categories = {}
-    for podcast in user_podcasts:
-        categories.setdefault(podcast.main_fields.category, 0)
-        categories[podcast.main_fields.category] += 1
+    sorted_categories = get_categories_count(user_podcasts)
+    podcasts = all_podcasts.exclude(Q(subscribe__user=user) | Q(main_fields__category=None)
+                    ).filter(main_fields__category__in=sorted_categories.keys())
+    podcasts_ids = podcasts.values_list("id", flat=True)
+    return list(podcasts_ids)
 
-    sorted_categories = [k for k,v in sorted(categories.items(), key=lambda item:item[1], reverse=True)]
+
+
 
     podcasts = all_podcasts.exclude(Q(subscribe__user=user) | Q(main_fields__category=None)).filter(main_fields__category__in=sorted_categories)
     podcasts_ids = podcasts.values_list("id", flat=True)
