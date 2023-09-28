0aa5f4d93ff816930dd2d4cb5075424e7f371a09
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-29 00:00:20 +0330

    feat: add like based recommendation

--- src/podcasts/utils.py
+++ src/podcasts/utils.py
@@ -28,7 +28,13 @@ def subscription_based_recommended_podcasts(user):
 
 
 
+def like_based_recomended_podcasts(user):
+    all_podcasts = PodcastRSS.objects.all()
+    user_liked_podcasts = all_podcasts.filter(podcastepisode__like__user=user)
 
-    podcasts = all_podcasts.exclude(Q(subscribe__user=user) | Q(main_fields__category=None)).filter(main_fields__category__in=sorted_categories)
+    sorted_categories = get_categories_count(user_liked_podcasts)
+
+    podcasts = all_podcasts.exclude(Q(subscribe__user=user) | Q(main_fields__category=None)
+                           ).filter(main_fields__category__in=sorted_categories)
     podcasts_ids = podcasts.values_list("id", flat=True)
     return list(podcasts_ids)
