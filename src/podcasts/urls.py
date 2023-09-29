a4193b9b7393d2168586f66b733665f8fcd06d28
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-30 02:52:22 +0330

    chore: add episode detail new urls

--- src/podcasts/urls.py
+++ src/podcasts/urls.py
@@ -9,13 +9,15 @@ urlpatterns = format_suffix_patterns([
     path("test/", test, name="test"),
 
     path('podcasts/', PodcastListView.as_view()),
-    path('podcast/<int:pk>', PodcastDetailView.as_view()),
+    path('podcast/<int:pk>/', PodcastDetailView.as_view()),
+
+    path('podcast/<int:rss_pk>/episode/<int:pk>/likes/', EpisodeDetailView.as_view({"get":"likes"})),
+    path('podcast/<int:rss_pk>/episode/<int:pk>/like/', EpisodeDetailView.as_view({"get":"like"})),
+    path('podcast/<int:rss_pk>/episode/<int:pk>/unlike/', EpisodeDetailView.as_view({"get":"unlike"})),
+    path('podcast/<int:rss_pk>/episode/<int:pk>/', EpisodeDetailView.as_view({"get":"get"})),
 
     path('podcast/<int:rss_pk>/episodes/', PodcastEpisodeListView.as_view()),
-    path('podcast/<int:rss_pk>/episode/<int:episode_nom>', PodcastEpisodeDetailView.as_view()),
 
     path('recommended/<str:method>/', PodcastRecommendationView.as_view()),
 
-    # re_path(r'podcast/test/(?P<pk>\d*)', PodcastTest.as_view(),),
-    # re_path(r'podcast/test/(?P<episode_nom>\d*)', PodcastTest.as_view(),),
 ])
