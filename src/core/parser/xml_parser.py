f33c10292e7a6b17ba9d94c36e5d403b5e47615f
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-04 03:57:32 +0330

    chore: return new_episodes in parser and podcast model update method

--- src/core/parser/xml_parser.py
+++ src/core/parser/xml_parser.py
@@ -90,6 +90,7 @@ class EpisodeXMLParser:
         if new_episodes:
             episodes = self.parse_multiple_episodes(new_episodes)
             self._create_episode_bulk(episodes)
+            return new_episodes
 
     def _create_episode_bulk(self, episode_objects:list):
         self.episode_model.objects.bulk_create(episode_objects)
