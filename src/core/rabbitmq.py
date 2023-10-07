a587bc1e55e191f3af1fdb9d786a69f2b2acc2f8
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-07 18:10:36 +0330

    chore: add rabbitmq publish_podcast_update function

--- src/core/rabbitmq.py
+++ src/core/rabbitmq.py
@@ -9,3 +9,7 @@ from config.settings import RABBIT_CHANNEL
 def publish(queue, method, body):
     properties = pika.BasicProperties(method)
     RABBIT_CHANNEL.basic_publish(exchange='', routing_key=queue, body=json.dumps(body), properties=properties)
+
+
+def publish_podcast_update(data):
+    return publish("podcast_update", "...", data)
