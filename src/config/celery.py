05f7dd10942b8837688c6572d7dcb69719281672
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-07 16:38:44 +0330

    feat: activate celery-beat podcast update task every 15min

--- src/config/celery.py
+++ src/config/celery.py
@@ -9,9 +9,12 @@ app = Celery("app")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
-# app.conf.beat_schedule = {
-#     'update-podcasts-every-hour': {
-#         'task': 'podcasts.tasks.update_podcasts_episodes',
-#         'schedule': crontab(minute=1),
-#     },
-# }
+app.conf.beat_schedule = {
+    'update-podcasts-every-hour': {
+        'task': 'podcasts.tasks.update_podcasts_episodes',
+        'schedule': crontab(minute="*/15"), # every 15 minutes
+        # 'schedule': crontab(minute=0), # every hour
+        # 'schedule': crontab(hour=3, minute=0), # 3:00 a.m
+    },
+}
+# app.conf.timezone
