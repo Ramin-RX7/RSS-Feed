3abeb11c24b065b0f7f9cd69b70455f9c42b0c66
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-10 15:27:55 +0330

    fix: create and close chanel in each rabbit publish

--- src/core/rabbitmq.py
+++ src/core/rabbitmq.py
@@ -2,14 +2,15 @@ import json
 
 import pika
 
-from config.settings import RABBIT_CHANNEL
+from config.settings import RABBIT_CHANNEL,RABBIT_CONNECTION
 
 
 
 def publish(queue, method, body):
     properties = pika.BasicProperties(method)
-    RABBIT_CHANNEL.basic_publish(exchange='', routing_key=queue, body=json.dumps(body), properties=properties)
-
+    ch = RABBIT_CONNECTION.channel()
+    ch.basic_publish(exchange='', routing_key=queue, body=json.dumps(body), properties=properties)
+    ch.close()
 
 def publish_podcast_update(data):
     return publish("podcast_update", "...", data)
