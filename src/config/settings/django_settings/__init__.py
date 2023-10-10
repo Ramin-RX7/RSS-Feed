03e168395f9e6313fbe3a84616a0d05819cd407c
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-11 00:25:52 +0330

    style: move django LOGGING to its own module

--- src/config/settings/django_settings/__init__.py
+++ src/config/settings/django_settings/__init__.py
@@ -4,6 +4,7 @@ from ..base import *
 
 from .apps import *
 from .email import *
+from .logging import *
 
 
 
@@ -90,33 +91,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 AUTH_USER_MODEL = "accounts.User"
-
-
-LOGGING = {
-    "version": 1,
-    'disable_existing_loggers': False,
-    "formatters": {
-        'verbose': {
-            'format': '[{levelname}] {asctime} | {message}',
-            'style': '{',
-        },
-    },
-    "handlers": {
-        'celery': {
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'celery.log',
-            'maxBytes': 1024 * 1024 * 10,  # 10MB
-            'backupCount': 3,
-            'formatter': 'verbose',
-        },
-    },
-    "loggers": {
-        'celery-logger': {
-            'handlers': ['celery'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    },
-}
-
