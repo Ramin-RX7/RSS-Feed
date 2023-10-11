e12633900f3ac2294b8c8791349d3ec51c870b76
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-12 01:26:41 +0330

    feat: register Notification and UserNotification to admin panel

--- src/interactions/admin.py
+++ src/interactions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Notification,UserNotification
+
+admin.site.register(Notification)
+admin.site.register(UserNotification)
