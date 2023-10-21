ba7779763b041e273b4da14a3d68cc7ef100244e
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-21 20:26:26 +0330

    feat: add auth_action to account.utils

---
+++ src/accounts/utils.py
@@ -0,0 +1,16 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import status
+from rest_framework.response import Response
+
+from .auth_backends import JWTAuthBackend
+
+
+
+def auth_action(func):
+    def wrapper(self, request, *args, **kwargs):
+        if not (auth:=JWTAuthBackend().authenticate(request)):
+            return Response({"details":_("login required")}, status=status.HTTP_403_FORBIDDEN)
+        request.user = auth[0]
+        request.payload = auth[1]
+        return func(self, request, *args, **kwargs)
+    return wrapper
