90f2e9eaf9d5c55083e13bf6dba6d3efcb203d46
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-21 19:07:09 +0330

    feat: add Episode model admin panel options

--- src/podcasts/admin.py
+++ src/podcasts/admin.py
@@ -1,14 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin,messages
+from django.urls import reverse
+from django.utils.html import format_html
 
 from . import models
-from . import forms
+from .tasks import update_podcast
 
 
 
-
-@admin.register(models.PodcastEpisode)
-class PodcastRSSAdmin(admin.ModelAdmin):
-    search_fields = ["id"]
 
 @admin.register(models.PodcastRSSPaths)
 class PodcastRSSAdmin(admin.ModelAdmin):
@@ -17,6 +15,31 @@ class PodcastRSSAdmin(admin.ModelAdmin):
 @admin.register(models.PodcastEpisodePaths)
 class PodcastRSSAdmin(admin.ModelAdmin):
     search_fields = ["id"]
+
+
+
+
+class EpisodeRSSFilter(admin.SimpleListFilter):
+    title = 'RSS'
+    parameter_name = 'rss'
+    def lookups(self, request, model_admin):
+        return [(rss.name,rss.name) for rss in models.PodcastRSS.objects.all()]
+    def queryset(self, request, queryset):
+        if rss_name:=self.value():
+            return queryset.filter(rss__name=rss_name)
+
+
+
+@admin.register(models.PodcastEpisode)
+class PodcastRSSAdmin(admin.ModelAdmin):
+    search_fields = ["id"]
+    list_per_page = 30
+    list_filter = [EpisodeRSSFilter,]
+    actions = ["update_rss_action", ]
+    search_fields = ["title"]
+
+
+
 
 
 
