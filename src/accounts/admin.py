5b83bff70dde6a10300a3550fc62cf0b11c2deb6
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-25 12:25:53 +0330

    feat: add user block/unblock to admin panel

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin,messages
 
 from .models import User,UserTracking
 from .forms import ChangeForm,UserAddForm
@@ -7,11 +7,32 @@ from .forms import ChangeForm,UserAddForm
 
 admin.site.register(UserTracking)
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "created_at"]
+    actions = ["block_user","unblock_user"]
 
     def get_form(self, request, obj=None, **kwargs):
         if obj is not None:
             return ChangeForm
         return UserAddForm
+
+    @admin.action(description="Blocks user login attempts to website")
+    def block_user(self, request, queryset):
+        for user in queryset:
+            user.block()
+        self.message_user(
+            request,
+            'User has been blocked successfully',
+            level=messages.SUCCESS
+        )
+
+    @admin.action(description="Unblock user login attempts")
+    def unblock_user(self, request, queryset):
+        queryset.update(is_active=True)
+        self.message_user(
+            request,
+            'User has been unblocked successfully',
+            level=messages.SUCCESS
+        )
