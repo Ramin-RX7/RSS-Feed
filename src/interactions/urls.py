1cad1697dac430a95691334bda021b6a3a8ddd1e
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-25 21:26:03 +0330

    style: remove extra code

--- src/interactions/urls.py
+++ src/interactions/urls.py
@@ -1,10 +1,1 @@
 from django.urls import path
-
-from .views import *
-
-
-urlpatterns = [
-    path("like/", LikeView.as_view(), name="like"),
-    path("comment/", CommentCreateView.as_view(), name="comment"),
-    path("subscribe/", SubscribeView.as_view(), name="subscribe"),
-]
