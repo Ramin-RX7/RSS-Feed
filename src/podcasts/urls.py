a0d0e4568a837de845896d14ab756d357174f497
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-25 11:03:17 +0330

    chore: remove podcast id in episode urls

--- src/podcasts/urls.py
+++ src/podcasts/urls.py
@@ -24,11 +24,11 @@ urlpatterns = format_suffix_patterns([
     path('podcast/<int:rss_pk>/episodes/', PodcastEpisodeListView.as_view(), name="podcast_episodes"),
     path('podcast/<int:rss_pk>/episode/<int:pk>/', EpisodeDetailView.as_view({"get":"get"}), name="episode_detail"),
 
-    path('podcast/<int:rss_pk>/episode/<int:pk>/like/', EpisodeDetailView.as_view({"get":"like"}), name="episode_like"),
-    path('podcast/<int:rss_pk>/episode/<int:pk>/unlike/', EpisodeDetailView.as_view({"get":"unlike"}), name="episode_unlike"),
-    path('podcast/<int:rss_pk>/episode/<int:pk>/likes/', EpisodeDetailView.as_view({"get":"likes"}), name="episode_likes"),
-    path('podcast/<int:rss_pk>/episode/<int:pk>/comment/', EpisodeDetailView.as_view({"post":"comment"}), name="episode_comment"),
-    path('podcast/<int:rss_pk>/episode/<int:pk>/comments/', EpisodeDetailView.as_view({"get":"comments"}), name="episode_comments"),
+    path('podcast/episode/<int:pk>/like/', EpisodeDetailView.as_view({"get":"like"}), name="episode_like"),
+    path('podcast/episode/<int:pk>/unlike/', EpisodeDetailView.as_view({"get":"unlike"}), name="episode_unlike"),
+    path('podcast/episode/<int:pk>/likes/', EpisodeDetailView.as_view({"get":"likes"}), name="episode_likes"),
+    path('podcast/episode/<int:pk>/comment/', EpisodeDetailView.as_view({"post":"comment"}), name="episode_comment"),
+    path('podcast/episode/<int:pk>/comments/', EpisodeDetailView.as_view({"get":"comments"}), name="episode_comments"),
 
 
     path('recommended/<str:method>/', PodcastRecommendationView.as_view(), name="recommendation"),
