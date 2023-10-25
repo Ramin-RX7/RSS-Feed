15bae8ba99ab2e43b8830a7465e9408f24f0ebac
Ramin RX7 <rawmin.rx@gmail.com>
2023-10-25 12:24:53 +0330

    chore: add logout method to User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,11 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.core.cache import caches
 from django.core.mail import send_mail
 from django.core.validators import MinLengthValidator,MaxLengthValidator
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 
 from core.models import BaseModel
 from .validators import username_validator
 from .managers import UserManager
+
+
+
+auth_cache = caches["auth"]
 
 
 
@@ -33,10 +38,15 @@ class User(AbstractBaseUser,PermissionsMixin,BaseModel):
     def __str__(self):
         return self.username
 
-
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def logout(self, jti:str) -> bool:
+        if auth_cache.get(f"{self.id}|{jti}"):
+            auth_cache.delete(f"{self.id}|{jti}")
+            return True
+        return False
 
 
 
