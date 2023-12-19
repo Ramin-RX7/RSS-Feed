e2ceee22e755b7d6871c897b9e39945d605cbab3
Ramin RX7 <rawmin.rx@gmail.com>
2023-12-20 02:05:41 +0330

    style: format `accounts/auth_backend` dir codes with black

--- src/accounts/auth_backends/jwt/utils.py
+++ src/accounts/auth_backends/jwt/utils.py
@@ -16,54 +16,54 @@ auth_cache = caches["auth"]
 
 def _generate_payload(username):
     return {
-        'username': username,
-        'iat': datetime.datetime.utcnow(),
-        'jti': uuid4().hex,
+        "username": username,
+        "iat": datetime.datetime.utcnow(),
+        "jti": uuid4().hex,
     }
+
 
 def _generate_refresh_token(base_payload):
-    return {
-        "token_type":"refresh",
-        "exp":REFRESH_TOKEN_EXPIRY,
-        **base_payload
-    }
+    return {"token_type": "refresh", "exp": REFRESH_TOKEN_EXPIRY, **base_payload}
+
 
 def _generate_access_token(base_payload):
-    return {
-        "token_type":"access",
-        "exp":ACCESS_TOKEN_EXPIRY,
-        **base_payload
-    }
+    return {"token_type": "access", "exp": ACCESS_TOKEN_EXPIRY, **base_payload}
 
 
 def generate_tokens(username):
     base_payload = _generate_payload(username)
     access_payload = _generate_access_token(base_payload)
     refresh_payload = _generate_refresh_token(base_payload)
-    return (base_payload["jti"], encode_payload(access_payload), encode_payload(refresh_payload))
+    return (
+        base_payload["jti"],
+        encode_payload(access_payload),
+        encode_payload(refresh_payload),
+    )
 
 
 def encode_payload(payload):
     return jwt.encode(payload, SECRET_KEY, algorithm=ENCRYPTION)
 
-def decode_jwt(token): # jwt.exceptions.DecodeError
+
+def decode_jwt(token):  # jwt.exceptions.DecodeError
     return jwt.decode(token, SECRET_KEY, algorithms=[ENCRYPTION])
 
 
-def _save_cache(user,jti,agent):
+def _save_cache(user, jti, agent):
     key = f"{user.id}|{jti}"
     value = f"{agent}"
-    auth_cache.set(key,value)
+    auth_cache.set(key, value)
 
 
 def _get_user_agent(headers):
     user_agent = headers.get("user-agent")
     if user_agent is None:
-        raise exceptions.ValidationError('user-agent header is not provided')
+        raise exceptions.ValidationError("user-agent header is not provided")
     return user_agent
+
 
 def _get_remote_addr(headers):
     user_agent = headers.get("user-agent")
     if user_agent is None:
-        raise exceptions.ValidationError('user-agent header is not provided')
+        raise exceptions.ValidationError("user-agent header is not provided")
     return user_agent
