2e7358c2bb978576f8838811d18ae53f16b0de94
Ramin RX7 <rawmin.rx@gmail.com>
2023-12-20 02:16:42 +0330

    style: format `config` dir codes with black formatter

--- src/config/celery.py
+++ src/config/celery.py
@@ -3,18 +3,17 @@ from celery import Celery
 from celery.schedules import crontab
 
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 
 app = Celery("app")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'update-podcasts-every-hour': {
-        'task': 'podcasts.tasks.update_podcasts_episodes',
-        'schedule': crontab(minute="*/15"), # every 15 minutes
-        # 'schedule': crontab(minute=0), # every hour
-        # 'schedule': crontab(hour=3, minute=0), # 3:00 a.m
+    "update-podcasts-every-hour": {
+        "task": "podcasts.tasks.update_podcasts_episodes",
+        "schedule": crontab(minute="*/15"),  # every 15 minutes
     },
 }
-# app.conf.timezone
--- src/config/settings/django_settings/logging.py
+++ src/config/settings/django_settings/logging.py
@@ -7,37 +7,37 @@ __all__ = ("LOGGING",)
 
 LOGGING = {
     "version": 1,
-    'disable_existing_loggers': False,
+    "disable_existing_loggers": False,
     "formatters": {
-        'verbose': {
-            'format': '[{levelname}] {asctime} | {message}',
-            'style': '{',
+        "verbose": {
+            "format": "[{levelname}] {asctime} | {message}",
+            "style": "{",
         },
     },
     "handlers": {
-        'celery': {
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'celery.log',
-            'maxBytes': 1024 * 1024 * 10,  # 10MB
-            'backupCount': 3,
-            'formatter': 'verbose',
+        "celery": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "celery.log",
+            "maxBytes": 1024 * 1024 * 10,  # 10MB
+            "backupCount": 3,
+            "formatter": "verbose",
         },
-        'elastic_handler': {
-            'level': 'DEBUG',
-            'class': 'config.settings.elastic.ElasticLogHandler',
-            'elastic_url': BASE_ENV("ELASTIC_URL"),
+        "elastic_handler": {
+            "level": "DEBUG",
+            "class": "config.settings.elastic.ElasticLogHandler",
+            "elastic_url": BASE_ENV("ELASTIC_URL"),
         },
     },
     "loggers": {
-        'celery-logger': {
-            'handlers': ['celery'],
-            'level': 'INFO',
-            'propagate': False,
+        "celery-logger": {
+            "handlers": ["celery"],
+            "level": "INFO",
+            "propagate": False,
         },
-        'elastic': {
-            'handlers':['elastic_handler'],
-            'level': 'DEBUG',
+        "elastic": {
+            "handlers": ["elastic_handler"],
+            "level": "DEBUG",
         },
     },
 }
